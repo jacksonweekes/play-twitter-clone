d231373b72dd2f726f5cdcdde45ef550a3cd6eef
Jackson Cleary <jacksonweekes@gmail.com>
2015-07-20 02:46:45 +1000

    Merge static-pages

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import play.*;
+import play.mvc.*;
 import model.BCryptExample;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.*;
 
 public class Application extends Controller {
 
@@ -14,8 +17,24 @@ public class Application extends Controller {
         return ok(views.html.application.matches.render(BCryptExample.matchesLastEncrypted(pw)));
     }
 
-    public static Result index() {
+    public static Result oldIndex() {
         return ok(views.html.application.index.render());
     }
 
+    // Static page controllers
+    public static Result index() {
+        return ok(index.render(""));
+    }
+
+    public static Result about() {
+        return ok(about.render());
+    }
+
+    public static Result help() {
+        return ok(help.render());
+    }
+
+    public static Result contact() {
+        return ok(contact.render());
+    }
 }

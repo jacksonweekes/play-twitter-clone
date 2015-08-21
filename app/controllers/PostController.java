1632a0c3fa05ae59b33495ef261caacd72227f4e
Jackson Cleary <jacksonweekes@gmail.com>
2015-08-21 19:58:32 +1000

    Complete post and search functionality

--- app/controllers/PostController.java
+++ app/controllers/PostController.java
@@ -3,6 +3,7 @@ package controllers;
 import model.Post;
 import model.PostDataInterface;
 import model.PostService;
+import model.User;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -25,6 +26,25 @@ public class PostController extends Controller {
         String message = values.get("message")[0];
         getPostService().addPost(new Post(username, message));
         return redirect(routes.UserController.showUser(username));
+    }
+
+    public static Result getPosts(String tag) {
+        return ok(views.html.posts.tag_page.render(tag));
+    }
+
+    public static Result search(String searchTerm, String type) {
+        if(type.equals("person")) {
+            User u = UserController.getUserService().getUserByUsername(searchTerm);
+            if(u != null) {
+                return redirect(routes.UserController.showUser(u.getUsername()));
+            } else {
+                flash("error", "User @" + searchTerm + " could not be found");
+                return redirect(routes.Application.index());
+            }
+        } else if(type.equals("topic")) {
+            return redirect(routes.PostController.getPosts(searchTerm));
+        }
+        return badRequest(views.html.static_pages.index.render(""));
     }
 
 }
